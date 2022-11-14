6a8e30beba05d5805fd3eee6e70dac926cff1f9d
William Spada <root@DESKTOP-HGD1NHP.localdomain>
2022-11-14 14:45:36 -0300

    finish api project

--- README.md
+++ README.md
@@ -1,20 +1,20 @@
 ### Requisitos
 
 - [x] - Deve ser possivel criar uma conta
-- [] - Deve ser possivel buscar o extrato bancario do cliente
-- [] - Deve ser possivel realizar um deposito
-- [] - Deve ser possivel realizar um saque
-- [] - Deve ser possivel buscar o extrato bancario do cliente por data
-- [] - Deve ser possivel atualizar dados da conta do cliente
-- [] - Deve ser possivel obter dados da conta do cliente
-- [] - Deve ser possivel deletar uma conta
+- [x] - Deve ser possivel buscar o extrato bancario do cliente
+- [x] - Deve ser possivel realizar um deposito
+- [x] - Deve ser possivel realizar um saque
+- [x] - Deve ser possivel buscar o extrato bancario do cliente por data
+- [x] - Deve ser possivel atualizar dados da conta do cliente
+- [x] - Deve ser possivel obter dados da conta do cliente
+- [x] - Deve ser possivel deletar uma conta
 
 
 ### Regras de negocio
 
 - [x] - Nao deve ser possivel cadastrar uma conta com CPF existente
-- [] - Nao deve ser possivel fazer um deposito em uma conta nao existente
-- [] - Nao deve ser possivel buscar o extrato em uma conta nao existente
-- [] - Nao deve ser possivel fazer um saque em uma conta nao existente 
-- [] - Nao deve ser possivel excluir uma conta nao existente
-- [] - Nao deve ser possivel fazer saque quando a conta tiver um saldo insuficiente
+- [x] - Nao deve ser possivel fazer um deposito em uma conta nao existente
+- [x] - Nao deve ser possivel buscar o extrato em uma conta nao existente
+- [x] - Nao deve ser possivel fazer um saque em uma conta nao existente 
+- [x] - Nao deve ser possivel fazer saque quando a conta tiver um saldo insuficiente
+- [x] - Nao deve ser possivel excluir uma conta nao existente
--- src/index.js
+++ src/index.js
@@ -6,12 +6,34 @@ const app = express();
 app.use(express.json());
 
 const customers = [];
-/* 
-    cpf - string
-    name - string
-    id - uuid
-    statement []
-*/
+
+// Middleware
+
+function verifyIfExistsCpfAccount(req, res, next) {
+    const { cpf } = req.headers;
+
+    const customer = customers.find(customer => customer.cpf === cpf);
+
+    if(!customer) {
+        return res.status(400).json({ error: "Customer not found" });
+    }
+
+    req.customer = customer;
+
+    return next();
+};
+
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if(operation.type === 'credit') {
+            return acc+operation.amount;
+        } else {
+            return acc-operation.amount;
+        }
+    }, 0);
+
+    return balance;
+};
 
 app.post('/account', (req, res) => {
     const { cpf, name } = req.body;
@@ -31,12 +53,84 @@ app.post('/account', (req, res) => {
     return res.status(201).send();
 });
 
-app.get('/statement/:cpf', (req, res) => {
-    const { cpf } = req.params;
-
-    const customer = customers.find(customer => customer.cpf === cpf);
-
+app.get('/statement', verifyIfExistsCpfAccount, (req, res) => {
+    const { customer } = req;
+     
     return res.status(200).json(customer.statement);
+});
+
+app.post('/deposit', verifyIfExistsCpfAccount, (req, res) => {
+    const { description, amount } = req.body;
+
+    const { customer } = req;
+
+    const statementOperation = {
+        description,
+        amount,
+        created_at: new Date(),
+        type: "credit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
+
+app.post('/withdraw', verifyIfExistsCpfAccount, (req, res) => {
+    const { amount } = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+
+    if(balance < amount) {
+        return res.status(400).json({ error: "Insufficient funds" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit",
+    };
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
+});
+
+app.get('/statement/date', verifyIfExistsCpfAccount, (req, res) => {
+    const { customer } = req;
+    const { date } = req.query;
+
+    const dateFormat = new Date(date+" 00:00");
+
+    const statement = customer.statement.filter((statement) => {
+        return statement.created_at.toDateString() === 
+            new Date(dateFormat).toDateString();
+    });
+    return res.status(200).json(statement);
+});
+
+app.put('/account', verifyIfExistsCpfAccount, (req, res) => {
+    const { name } = req.body;
+    const { customer } = req;
+
+    customer.name = name;
+    
+    return res.status(201).send();
+});
+
+app.get('/account', verifyIfExistsCpfAccount, (req, res) => {
+    const { customer } = req;
+
+    return res.status(200).json(customer);
+});
+
+app.delete('/account', verifyIfExistsCpfAccount, (req, res) => {
+    const { customer } = req;
+
+    customers.splice(customer, 1);
+
+    return res.status(200).json(customers);
 });
 
 app.listen(3333, () => {
